208e6b4b483fdd6688d1f650430bb0c180bcb7b2
liaohaha <645741292@qq.com>
2023-07-02 11:40:27 +0800

    first_hello_world_0702

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        // this is a test java project
         System.out.println("Hello World"); // 输出 Hello World
-
+        Integer x = new Integer(1234);
+        System.out.println(x);
+        Integer m = 123;
+        Integer n = 123;
+        System.out.println(m == n); // true
     }
 }
